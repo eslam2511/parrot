9583b276d2a710392f1e260bf223ab2089293141
eslam2511 <111900405+eslam2511@users.noreply.github.com>
2022-09-09 17:19:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sidebar_menu = document.getElementById("side-menu");
+document.querySelector(".menu-button").onclick = function(){
+     sidebar_menu.style.left = "0px"
+    
+}
+
+document.querySelector(".whole").onclick = function(){
+    sidebar_menu.style.left = "-100%"
+}
+
+    
+
